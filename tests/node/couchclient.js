71eacccc7d396506d95ed380750c7647d023e305
nrstott <nrstott@gmail.com>
2010-07-29 05:54:35 -0700

    incremental progress on changing to promises

--- tests/node/couchclient.js
+++ tests/node/couchclient.js
@@ -5,7 +5,8 @@ var
   TEST_DOC = { hello: "world" },
   assert   = require("assert"),
   couchdb  = require("../../lib/couchdb"),
-  when     = require("promised-io/promise").when,
+  Q        = require("promised-io/promise"),
+  when     = Q.when,
   client   = couchdb.createClient({ 
                port: 5984, 
                host: "192.168.15.52", 
@@ -29,6 +30,136 @@ exports["should get all dbs"] = function() {
     assert.ok(false, err);
   });
 };
+
+exports["should get UUIDs"] = function() {
+  var count  = 5;
+  
+  return when(client.uuids(count), function(resp) {
+    assert.notEqual(null, resp.uuids, "resp.uuids should not be null.  "+JSON.stringify(resp));
+    assert.equal(count, resp.uuids.length, "Expected "+count+" uuids.  "+JSON.stringify(resp.uuids));
+  });
+};
+
+exports["shold get config"] = function() {
+  return when(client.config()
+  , function(resp) {
+    assert.notEqual(null, resp);
+  }
+  , function(err) { assert.ok(false, err); });
+};
+
+exports["should create and remove db"] = function() {
+  var db = client.db(DB_NAME);
+  
+  return when(db.create(), 
+    function success(resp) {
+      assert.notEqual(null, resp);
+      assert.ok(resp.ok, JSON.stringify(resp));
+    
+      return when(db.remove(), 
+        function(resp) {
+        assert.notEqual(null, resp);
+        assert.ok(resp.ok, JSON.stringify(resp));
+      });
+    }, 
+    function error(err) {
+      assert.ok(false, err);
+    });
+};
+
+exports["should get states"] = function() {
+  return when(client.stats(),
+    function(resp) {
+      assert.notEqual(null, resp.couchdb);
+    });
+};
+
+exports["should replicate"] = function() {
+  var 
+    db1 = client.db(DB_NAME),
+    db2 = client.db(DB_NAME2);
+  
+  return when(
+    Q.all([db1.create(), db2.create()]),
+    function() {
+      return when(
+        client.replicate(DB_NAME, DB_NAME2),
+        function(resp) {
+          console.log("replicate response:"+JSON.stringify(resp));
+          assert.ok(resp);
+          assert.ok(!resp.error);
+        },
+        function(err) {
+          assert.ok(false, err);
+        }
+      )
+    }
+  );
+};
+
+(function() {
+  var 
+    self = this, 
+    db;
+    
+  function before() {
+    db = client.db(DB_NAME);
+    return when(db.exists(), function(exists) {
+      console.log("exists:"+exists);
+      if (exists) {
+        return when(db.remove(), function() { 
+          return db.create();
+        });
+      } else {
+        return db.create();
+      }
+    });
+  }
+  
+  function after() {
+    return db.remove();
+  }
+  
+  var tests = {
+    "should have no documents": function() {
+      console.log("allDocs");
+      return when(db.allDocs(), function(resp) {
+        assert.ok(Array.isArray(resp.rows));
+        assert.equal(0, resp.rows.length);
+      });
+    }, 
+    "should create document with id": function() {
+      return when(db.saveDoc({ _id: "ABC123", hello: "world" }), function(resp) {
+        assert.notEqual(null, resp);
+        assert.ok(resp.ok);
+      });
+    }, 
+    "should create document without id": function() {
+      return when(db.saveDoc({ hello: "world" }), 
+        function(resp) {
+          console.log(JSON.stringify(resp));
+          assert.ok(resp.ok);
+        });
+    }
+  };
+    
+  exports["database tests"] = {};
+  
+  for (var test in tests) {
+    exports["database tests"][test] = function() {
+      return when(before(), function() {
+        return when(tests[test](), 
+          function() {
+            return after();
+          }, 
+          function(err) {
+            assert.ok(false, err);
+            after();
+          });
+        });
+    };
+  }
+})();
 
 if (require.main == module) {
   require("patr/runner").run(exports);
@@ -54,24 +185,8 @@ if (require.main == module) {
     assert.notEqual(null, response);
   });
 };*/
-/*
-exports["should get UUIDs"] = function(assert, beforeExit) {
-  var 
-    count  = 5, 
-    hasRun = false;
-  
-  client.uuids(count, function(err, resp) {
-    hasRun = true;
-    assert.notEqual(null, resp);
-    assert.equal(null, err);
-    assert.equal(count, resp.uuids.length, sys.puts(JSON.stringify(resp)));
-  });
-  
-  beforeExit(function() {
-    assert.ok(hasRun, "Callback should've run");
-  })
-};
 
+/*
 exports["should not find database that does not exist"] = function(assert, beforeExit) {
   var hasRun = false;
   
